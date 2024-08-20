cb4c99a661b004b86296c496ab9775307186ff6f
guilherme bonato <guilhermeferrazbonato@gmail.com>
2024-08-20 10:37:57 -0300

    Rotas protegidas por autenticação JWT

--- src/routers/userRoute.ts
+++ src/routers/userRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/userController';
+import { registerUser, loginUser, getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/userController';
 import { authenticateToken } from '../authMiddleware/authMiddleware';
 
 const router = Router();
@@ -9,5 +9,11 @@ router.post('/register', registerUser);
 
 // Rota para login de usuários
 router.post('/login', loginUser);
+
+// Rotas protegidas por autenticação JWT
+router.get('/getAll', authenticateToken, getAllUsers); // Buscar todos os usuários
+router.get('/:id', authenticateToken, getUserById); // Buscar um usuário por ID
+router.put('/:id', authenticateToken, updateUser); // Atualizar um usuário
+router.delete('/:id', authenticateToken, deleteUser); // Deletar um usuário
 
 export default router;
